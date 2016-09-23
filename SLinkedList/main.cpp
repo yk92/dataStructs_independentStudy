ecd74689eb4502fd914504ace786f14f16668462
Yuval Klein <yk92@njit.edu>
2016-09-23 16:46:40 -0400

    Initial Commit

---
+++ SLinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SLinkedList
+//
+//  Created by Yuval Klein on 9/23/16.
+//  Copyright © 2016 Yuval Klein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
